264c8e1315c318911f507d486915536dc2471f0f
Jason Robinson <jaywink@basshero.org>
2013-07-01 23:17:43 +0300

    Add no-op mode and ctrl-c option to quit script while running

--- migrate.py
+++ migrate.py
@@ -26,6 +26,7 @@ def arguments():
     parser = argparse.ArgumentParser(description=_description_)
     parser.add_argument('sourcepod', help='username:password@https://sourcepod.tld')
     parser.add_argument('targetpod', help='username:password@https://targetpod.tld')
+    parser.add_argument('-n', action='store_true', help="Don't do any changes, just print out")
     return parser.parse_args()
 
 def connect(connstr):
@@ -85,9 +86,17 @@ def migrate_contacts(args):
             if person['id'] == 0:
                 raise Exception()
             try:
-                add_to_aspect(pods[1], person['id'], aspect_id)
-                print("ADDED",user['handle'],person['id'])
+                if not args.n:
+                    add_to_aspect(pods[1], person['id'], aspect_id)
+                    print("ADDED",user['handle'],person['id'])
+                    
+                else:
+                    print("[NO-OP mode] ADDED",user['handle'],person['id'])
                 counts['added'] += 1
+            except KeyboardInterrupt, e:
+                # User wants out
+                close_connections(pods)
+                return counts
             except Exception, e:
                 print(e.message)
                 if e.message.find('400') > -1:
@@ -99,7 +108,11 @@ def migrate_contacts(args):
                 else:
                     counts['unknownerrors'] += 1
                     print("ERROR - unknown error")
-        except:
+        except KeyboardInterrupt, e:
+            # User wants out
+            close_connections(pods)
+            return counts
+        except Exception, e:
             print("ERROR - could not find user")
             counts['notfound'] += 1
     close_connections(pods)
